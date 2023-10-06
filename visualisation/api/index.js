04a01cd26562b93863519cf075e585abd7c3bea7
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2023-10-06 10:05:13 +0200

    Architecture de la partie visualisation

---
+++ visualisation/api/index.js
@@ -0,0 +1,14 @@
+// variables
+const dotenv = require('dotenv');
+const express = require('express');
+
+const server = express();
+
+// utilisation des variables d'environnement
+dotenv.config();
+
+// lancement du serveur
+server.use(express.json());
+server.listen(process.env.PORT_SERVER, () => {
+    console.log(`Server is listening port ${process.env.PORT_SERVER}`);
+});
