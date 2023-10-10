95e7f6108c6611d99bdeee6e68644d21122da2d8
Mathéo <matheo.girard@edu.univ-fcomte.fr>
2023-10-10 17:32:33 +0200

    Conteneur grapique ajusté à la bonne taille

--- visualisation/frontend/src/components/GraphBarComponent.vue
+++ visualisation/frontend/src/components/GraphBarComponent.vue
@@ -1,50 +1,50 @@
 <template>
     <div>
-        <FilterComponent v-if="filter" :stimulis="stimulis" />
-        <br>
+      <!-- Afficher le composant de filtre si "filter" est vrai -->
+      <FilterComponent v-if="filter" :stimulis="stimulis" />
+  
+      <!-- Conteneur du graphique avec une hauteur maximale et défilement vertical si nécessaire -->
+      <div class="chart-container">
         <p>Graphique component {{ donnees }}</p>
         <Bar :data="donnees" :options="options" />
+      </div>
     </div>
-</template>
-
-
-<script>
-import {
-  Chart,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
-import { Bar } from 'vue-chartjs'
-import FilterComponent from '@/components/FilterComponent.vue'
-
-Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
-
-export default {
+  </template>
+  
+  <script>
+  import { Chart, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  import { Bar } from 'vue-chartjs'
+  import FilterComponent from '@/components/FilterComponent.vue'
+  
+  Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+  
+  export default {
     name: 'GraphBarComponent',
     components: {
-        Bar,
-        FilterComponent
+      Bar,
+      FilterComponent
     },
     props: {
-        donnees: {},
-        filter: Boolean
-        // label_abscisse: String,
-        // label_ordonnee: String,
-        // titre: String
+      donnees: Object, 
+      filter: Boolean
     },
-    data: () => ({
+    data() {
+      return {
         options: {
-            responsive: true,
-            maintainAspectRatio: false
+          responsive: true,
+          maintainAspectRatio: false
         },
         stimulis: ["sonor", "visuel"]
-    })
-}
-</script>
-
-
-<style scoped></style>
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Style pour le conteneur du graphique */
+  .chart-container {
+    max-height: 400px; 
+    overflow-y: auto; 
+  }
+  </style>
+  
