236223bcfb2ac259cac4f1fc379b297550ce739d
messaline <messa@b.com>
2023-11-06 17:15:27 +0100

    add result to user

--- visualisation/api/routers/server-tcp_user.router.js
+++ visualisation/api/routers/server-tcp_user.router.js
@@ -57,4 +57,73 @@ const router = express.Router();
  */
 router.post("/add", controller.createUser);
 
+/**
+ *  @swagger
+ *  /user/result/add:
+ *    post:
+ *      tags:
+ *        - USERS
+ *      description: Create new result with specified properties to insert the result into a specific user.
+ *      parameters:
+ *        - in: body
+ *          name: result
+ *          description: The result to create.
+ *          schema:
+ *            type: object
+ *            required:
+ *              - result
+ *              - user
+ *            properties:
+ *              result:
+ *                type: object
+ *                properties:
+ *                  experience:
+ *                    type: string
+ *                  reactTime:
+ *                    type: number
+ *                  execTime:
+ *                    type: number
+ *              user:
+ *                type: object
+ *                properties:
+ *                  name:
+ *                    type: string
+ *                  firstName:
+ *                    type: string
+ *                  password:
+ *                    type: string
+ *                  email:
+ *                    type: string
+ *                  age:
+ *                    type: number
+ *                  gender:
+ *                    type: string
+ *                  typeUser:
+ *                    type: string
+ *                  results:
+ *                    type: array
+ *            example:
+ *              result:
+ *                experience: "457870657269656e63652031"
+ *                reactTime: 10
+ *                execTime: 5
+ *              user:
+ *                name: John
+ *                firstName: Doe
+ *                password: mypassword
+ *                email: john@example.com
+ *                age: 30
+ *                gender: Masculin
+ *                typeUser: cobaye
+ *                results: []
+ *      responses:
+ *        '200':
+ *          description: User created succeffuly
+ *        '449':
+ *          description: User or email are already registered
+ *        '500':
+ *          description: Internal server error
+ */
+router.post("/result/add", controller.addResult);
+
 module.exports = router;
