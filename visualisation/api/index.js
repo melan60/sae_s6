14f9e231a7e397e078d76b8e3f8f2e36fdbe7791
Mélanie <melan60@free.fr>
2023-11-06 16:05:25 +0100

    Updated docker-compose

--- visualisation/api/index.js
+++ visualisation/api/index.js
@@ -60,8 +60,10 @@ server.use("/user", routes_server_tcp_user);
 
 function initialisationBDD() {
   let dev_db_url;
-  if(process.env.PORT_SERVER)
+  if(process.env.DOCKER_MONGO){
     dev_db_url = `mongodb://mongodb:27017/${process.env.DATABASE_NAME}`
+    console.log("mongo")
+   }
   else
     dev_db_url = `mongodb://127.0.0.1/${process.env.DATABASE_NAME}`;
   mongoose.connect(dev_db_url)
