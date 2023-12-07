99c2e8fa5f115ce8fdd4317b8a4ab7cfb02086e0
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2023-12-07 15:35:28 +0100

    Suppression du code mort

--- visualisation/frontend/src/store/index.js
+++ visualisation/frontend/src/store/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: null, // { _id: '655f6ac4b7eb21e278e04b34' },
+    user: null, // { _id: '656853171475ca3c16ad187c' },
     stimuli: null
   },
   getters: {
@@ -22,32 +22,16 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    async login({commit}, credentials) {
-        const response = await axios.post('/api/login', credentials);
-        // Store the token in localStorage
-        localStorage.setItem('token', response.data.token);
+    async login({ commit }, credentials) {
+      const response = await axios.post('/api/login', credentials);
+      // Store the token in localStorage
+      localStorage.setItem('token', response.data.token);
 
-        // Commit the user data to the store
-        commit('setUser', response.data.user);
-        console.log('login', response.data.user);
+      // Commit the user data to the store
+      commit('setUser', response.data.user);
+      console.log('login', response.data.user);
 
-        return response.data;
-    },
-    async fetchUserData({ commit }, nomCobaye) {
-      try {
-        console.log('fetchUserData', nomCobaye);
-        const response = await axios.get(`/api/cobaye/${nomCobaye}`, {
-          headers: {
-            Authorization: 'Bearer ' + localStorage.getItem('token')
-          }
-        });
-        commit('setUser', response.data.user);
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
+      return response.data;
     }
-  },
-
-  modules: {
   }
-})
+});
--- visualisation/frontend/src/views/HomeView.vue
+++ visualisation/frontend/src/views/HomeView.vue
@@ -1,20 +1,10 @@
 <template>
-  <!--  <div class="main-container">
-      <div class="container-graph" v-for="(value, index) in values" :key="index">
-        <div class="graph-container">
-          <GraphBarComponent v-if="index < 2" :data="value" :options="options"/>
-          <GraphLineComponent v-if="index > 1" :data="value" :options="options"/>
-        </div>
-      </div>-->
-  <div>
-    <div class="grid-container">
-      <div v-for="(value, index) in values" :key="index">
-        <GraphBarComponent v-if="index < 3" :data="value" :options="options"/>
-        <GraphLineComponent v-else :data="value" :options="options"/>
-      </div>
+  <div class="grid-container">
+    <div v-for="(value, index) in values" :key="index">
+      <GraphBarComponent v-if="index < 3" :data="value" :options="options" />
+      <GraphLineComponent v-else :data="value" :options="options" />
     </div>
   </div>
-
 </template>
 
 <script>
@@ -29,67 +19,6 @@ export default {
     GraphLineComponent
   },
   data: () => ({
-    chartData: {
-      labels: ['January', 'February', 'March', 'April', 'May'],
-      datasets: [
-        {
-          label: 'Scatter Dataset 1',
-          fill: false,
-          borderColor: '#308bf4',
-          backgroundColor: ['#7fdbe8', '#35a9a0', '#294f40', '#62e07f', '#2c3e50', '#6bc2e0', '#308bf4'],
-          data: [
-            {
-              x: -2,
-              y: 4
-            },
-            {
-              x: -1,
-              y: 1
-            },
-            {
-              x: 0,
-              y: 0
-            },
-            {
-              x: 1,
-              y: 1
-            },
-            {
-              x: 2,
-              y: 4
-            }
-          ]
-        },
-        {
-          label: 'Scatter Dataset 2',
-          fill: false,
-          borderColor: '#7acbf9',
-          backgroundColor: '#7acbf9',
-          data: [
-            {
-              x: -2,
-              y: -4
-            },
-            {
-              x: -1,
-              y: -1
-            },
-            {
-              x: 0,
-              y: 1
-            },
-            {
-              x: 1,
-              y: -1
-            },
-            {
-              x: 2,
-              y: -4
-            }
-          ]
-        }
-      ]
-    },
     values: [],
     options: {
       responsive: true,
@@ -101,7 +30,7 @@ export default {
     const user = this.$store.getters.getUser;
     try {
       const res = user ? await axios.get(`http://localhost:5000/graphs/users?id_user=${user._id}`) :
-          await axios.get("http://localhost:5000/graphs/time");
+        await axios.get("http://localhost:5000/graphs/time");
       this.initGraph(res.data.data);
     } catch (e) {
       console.log(e);
@@ -117,19 +46,19 @@ export default {
         labels = value.labels || (value.first && value.first.labels);
 
         datasets = value.first
-            ? Object.values(value).map((item, id) => {
-              return ({
-                label: item.title,
-                backgroundColor: colors[id % colors.length],
-                borderColor: colors[id % colors.length],
-                data: item.data
-              })
-            }) : [{
-              label: value.title,
-              backgroundColor: colors[index % colors.length],
-              borderColor: colors[index % colors.length],
-              data: value.data
-            }];
+          ? Object.values(value).map((item, id) => {
+            return ({
+              label: item.title,
+              backgroundColor: colors[id % colors.length],
+              borderColor: colors[id % colors.length],
+              data: item.data
+            })
+          }) : [{
+            label: value.title,
+            backgroundColor: colors[index % colors.length],
+            borderColor: colors[index % colors.length],
+            data: value.data
+          }];
 
         this.values.push({
           labels: labels,
@@ -148,31 +77,6 @@ export default {
 
 
 <style scoped>
-.main-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-}
-
-.container-graph {
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-}
-
-.graph-container {
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 20px;
-  margin: 10px;
-  width: calc(50% - 20px);
-  box-sizing: border-box;
-}
-
 .grid-container {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
