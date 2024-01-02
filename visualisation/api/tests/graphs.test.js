0ed1370284261ee6f0cee2b3eddec87cb2dfb744
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-01-02 14:23:47 +0100

    Tests réalisés sur le service getIndividualData

--- visualisation/api/tests/graphs.test.js
+++ visualisation/api/tests/graphs.test.js
@@ -1,6 +1,11 @@
+const assert = require('assert');
 const mongoose = require('mongoose');
 
 const db = require('../database/db.init');
+const { User } = require('../models/index_models');
+
+const controller = require('../controllers/front_graphs_controller');
+const service = require('../services/front_graphs_service');
 
 
 before(async () => {
@@ -17,4 +22,38 @@ before(async () => {
             await db.initBdD();
         })
         .catch(e => console.error(e)); // server
+});
+
+
+describe('Testing the getIndividualData service', function () {
+
+    it('The service returns an array of length 2 with the results of the cobaye', async function () {
+        const user = await User.findOne({ email: "patelaiden@gmail.com" });
+        const results = await new Promise((resolve, reject) => {
+            service.getIndividualData(user._id, (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            });
+        });
+
+        const expectedLength = 2;
+        assert.strictEqual(results.length, expectedLength, `Length array ${expectedLength} expected ; got length array ${results.length}`);
+
+        const expectedData = user.results.map(exp => exp.error);
+        assert.deepEqual(results[1].data, expectedData, `Expected error ${expectedData}; got ${results[1].data}`);
+    });
+
+    it('The service returns an error related to the user id', async function () {
+        const results = await new Promise((resolve, reject) => {
+            service.getIndividualData(1, (error, result) => {
+                if (error) {
+                    resolve(error);
+                }
+                reject(result)
+            });
+        });
+        assert.ok(results);
+    });
 });
