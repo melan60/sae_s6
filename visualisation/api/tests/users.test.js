98ef6e14b18dc482f3100b7645a4e995995cfe46
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-01-04 11:06:07 +0100

    Commentaires enlevés

--- visualisation/api/tests/users.test.js
+++ visualisation/api/tests/users.test.js
@@ -40,86 +40,86 @@ before(() => {
 // ============================================================================ createUser
 describe('Testing the createUser service', function () {
 
-    // it('the service creates a user', async function () {
-    //     const user_created = await new Promise((resolve, reject) => {
-    //         service.createUser(user, (error, result) => {
-    //             if (error) {
-    //                 reject(error);
-    //             }
-    //             resolve(result);
-    //         });
-    //     });
+    it('the service creates a user', async function () {
+        const user_created = await new Promise((resolve, reject) => {
+            service.createUser(user, (error, result) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(result);
+            });
+        });
 
-    //     // tester que l'user renvoyé a les mêmes attributs que les paramètres passés
-    //     assert.equal(user_created._doc.name, user.name);
+        // tester que l'user renvoyé a les mêmes attributs que les paramètres passés
+        assert.equal(user_created._doc.name, user.name);
 
-    //     // tester que l'user existe dans la BDD
-    //     const user_founded = await User.find({ email: user.email });
-    //     assert.ok(user_founded);
+        // tester que l'user existe dans la BDD
+        const user_founded = await User.find({ email: user.email });
+        assert.ok(user_founded);
 
-    //     // tester que le mot de passe est crypté et qu'une fois décrypté, c'est le même que celui souhaité
-    //     assert.notEqual(user_founded[0].password, user.password); // TODO
-    //     assert.ok(bcrypt.compare(user_founded[0].password, user.password));
-    // });
+        // tester que le mot de passe est crypté et qu'une fois décrypté, c'est le même que celui souhaité
+        assert.notEqual(user_founded[0].password, user.password); // TODO
+        assert.ok(bcrypt.compare(user_founded[0].password, user.password));
+    });
 
-    // it('the service returns an error because the user is already registered', async function () {
-    //     const result = await new Promise((resolve, reject) => {
-    //         service.createUser(user, (error, result) => {
-    //             if (error) {
-    //                 resolve(error);
-    //             }
-    //             reject(result);
-    //         });
-    //     });
+    it('the service returns an error because the user is already registered', async function () {
+        const result = await new Promise((resolve, reject) => {
+            service.createUser(user, (error, result) => {
+                if (error) {
+                    resolve(error);
+                }
+                reject(result);
+            });
+        });
 
-    //     assert.equal(result, common_variables.already_registered);
-    // });
+        assert.equal(result, common_variables.already_registered);
+    });
 
-    // it('the service returns age error', async function () {
-    //     const badUser = {
-    //         name: "GIRARD",
-    //         firstName: "Mathéo",
-    //         password: "19500#cde",
-    //         email: "matheo.girard@edu.univ-fcomte.fr",
-    //         age: "Bébé",
-    //         gender: "Masculin",
-    //         typeUser: "cobaye"
-    //     };
+    it('the service returns age error', async function () {
+        const badUser = {
+            name: "GIRARD",
+            firstName: "Mathéo",
+            password: "19500#cde",
+            email: "matheo.girard@edu.univ-fcomte.fr",
+            age: "Bébé",
+            gender: "Masculin",
+            typeUser: "cobaye"
+        };
 
-    //     const result = await new Promise((resolve, reject) => {
-    //         service.createUser(badUser, (error, result) => {
-    //             if (error) {
-    //                 resolve(error);
-    //             }
-    //             reject(result);
-    //         });
-    //     });
+        const result = await new Promise((resolve, reject) => {
+            service.createUser(badUser, (error, result) => {
+                if (error) {
+                    resolve(error);
+                }
+                reject(result);
+            });
+        });
 
-    //     assert.ok(result);
-    // });
+        assert.ok(result);
+    });
 
-    // it('the service returns gender error', async function () {
-    //     const badUser = {
-    //         name: "GIRARD",
-    //         firstName: "Mathéo",
-    //         password: "19500#cde",
-    //         email: "matheo.girard@edu.univ-fcomte.fr",
-    //         age: "Adulte",
-    //         gender: "Non spécifié",
-    //         typeUser: "cobaye"
-    //     };
+    it('the service returns gender error', async function () {
+        const badUser = {
+            name: "GIRARD",
+            firstName: "Mathéo",
+            password: "19500#cde",
+            email: "matheo.girard@edu.univ-fcomte.fr",
+            age: "Adulte",
+            gender: "Non spécifié",
+            typeUser: "cobaye"
+        };
 
-    //     const result = await new Promise((resolve, reject) => {
-    //         service.createUser(badUser, (error, result) => {
-    //             if (error) {
-    //                 resolve(error);
-    //             }
-    //             reject(result);
-    //         });
-    //     });
+        const result = await new Promise((resolve, reject) => {
+            service.createUser(badUser, (error, result) => {
+                if (error) {
+                    resolve(error);
+                }
+                reject(result);
+            });
+        });
 
-    //     assert.ok(result);
-    // });
+        assert.ok(result);
+    });
 });
 
 
